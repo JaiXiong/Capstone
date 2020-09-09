1d33707017d76e77ef7df3fa965f2db1e9ef6d1c
VectorShadow <ncbrayshaw@hotmail.com>
2020-09-09 10:04:16 -0500

    Added printString method.

--- src/Driver.java
+++ src/Driver.java
@@ -1,12 +1,22 @@
 import java.awt.*;
 
 public class Driver {
+
+    private static final Console CONSOLE =
+            new Console(24, 64, new Dimension(9, 16));
+
     public static void main(String[] args) {
-        Console console = new Console(24, 64, new Dimension(9, 16));
-        console.update(1, 1, 'T');
-        console.update(1, 2, 'e');
-        console.update(1, 3, 's');
-        console.update(1, 4, 't');
-        console.refresh();
+        printString(1, 1, "Test");
+    }
+
+    public static void printString(int row, int initialColumn, String displayString) {
+        int currentColumn;
+        for (int i = 0; i < displayString.length(); ++i) {
+            currentColumn = initialColumn + i;
+            if (CONSOLE.validatePosition(row, currentColumn))
+                CONSOLE.update(row, currentColumn, displayString.charAt(i));
+            else break;
+        }
+        CONSOLE.refresh();
     }
 }
