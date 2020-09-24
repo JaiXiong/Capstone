cd309938ae54af5cc11f4561b5347176c5950689
Joe Calamia <jcalamia@uwm.edu>
2020-09-24 16:26:17 -0500

    Added initial character structures

---
+++ src/main/character/AbstactNonPlayerCharacter.java
@@ -0,0 +1,72 @@
+package main.character;
+
+public abstract class AbstactNonPlayerCharacter extends AbstractCharacter{
+
+    /* characters may need the information on how they display
+     * (which char, what color, what background) included in
+     * the character class
+     */
+
+    //TODO
+    /* for now the getters will just give raw stats
+     * but if we add persistant effects we'll have to
+     * figure those in
+     */
+    @Override
+    public int getMoveRate() {
+        return moveRate;
+    }
+
+    @Override
+    public double getOffenseA() {
+        return offenseA;
+    }
+
+    @Override
+    public double getOffenseB() {
+        return offenseB;
+    }
+
+    @Override
+    public double getAccuracy() {
+        return accuracy;
+    }
+
+    @Override
+    public double getEvade() {
+        return evade;
+    }
+
+    @Override
+    public double getResistA() {
+        return resistA;
+    }
+
+    @Override
+    public double getResistB() {
+        return resistB;
+    }
+
+    @Override
+    public double getResistC() {
+        return resistC;
+    }
+
+    @Override
+    public double getResistD() {
+        return resistD;
+    }
+
+    public int getXP(){
+        return xp;
+    }
+
+    //TODO
+    /* this method scripts what actions an NPC
+     * takes on their turn
+     */
+    public void executeTurn(){
+    }
+
+    //TODO add some other actions
+}
---
+++ src/main/character/AbstractCharacter.java
@@ -0,0 +1,121 @@
+package main.character;
+
+public abstract class AbstractCharacter {
+
+    /* characters may need the information on how they display
+     * (which char, what color, what background) included in
+     * the character class
+     */
+
+    /* to avoid initiative collision, each initiativeID should be unique,
+     * which also lets us use it as a character hash
+     */
+    int initiativeID;
+
+    /* contains all the actions a given character has available
+     * beyond the handful of default actions
+     */
+    String[] actions;
+
+    /* all characters have XP, but it works different
+     * for the player than for NPCs. NPC XP doesn't
+     * change, but player XP increases based on the
+     * XP of NPCs they defeat. Only needs a getter
+     * for NPCs.
+     */
+    int xp;
+
+    // basic character stats
+    int health;
+    int energy;
+    int moveRate;
+
+    // percentage stats
+    // higher is better for offense
+    double offenseA;
+    double offenseB;
+    double accuracy;
+    //lower is better for defense
+    double evade;
+    double resistA;
+    double resistB;
+    double resistC;
+    double resistD;
+
+
+    /* helper method for building the actions list so its
+     * not all crammed in the constructor
+     */
+    abstract String[] buildActions();
+
+
+    /* initiativeID shouldn't be modified
+     * health and energy should only be modified by events,
+     * not constant modifiers, so we can put the getters here
+     */
+    public int getInitiativeID(){
+        return initiativeID;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public int getEnergy(){
+        return energy;
+    }
+
+    /* the remaining stats can be modified by persistant
+     * effects, and have to calculate a total value based
+     * off the fields
+     */
+    abstract int getMoveRate();
+
+    abstract double getOffenseA();
+
+    abstract double getOffenseB();
+
+    abstract double getAccuracy();
+
+    abstract double getEvade();
+
+    abstract double getResistA();
+
+    abstract double getResistB();
+
+    abstract double getResistC();
+
+    abstract double getResistD();
+
+    //TODO needs to be filled in
+    /* @param amount how much unmitigated damage is being dealt
+     * @param type which resistance applies to the damage
+     * @param chanceToHit the attacker's accuracy
+     * actions that do damage to a character call that character's
+     * takeDamage to modify that character's health.
+     */
+    public void takeDamage(int amount, String type, double chanceToHit){
+    }
+
+    /*TODO fill in amount calculation, figure out if targetting needs to be
+     * within the method or a seperate method.
+     */
+    /* @param the character getting hit
+     * calculates base damage and calls takeDamage
+     */
+    public void basicOffense(AbstractCharacter target){
+        int amount = 0;
+        target.takeDamage(amount, "offenseA", accuracy);
+    }
+
+    //TODO needs to be filled in
+    /* action that moves the character
+     *
+     */
+    public void move(){
+    }
+
+    //TODO possibly implement persistant effects (buffs/debuffs, damage-over-time, etc)
+
+    //TODO possibly implement interaction with non-character objects like doors
+}
