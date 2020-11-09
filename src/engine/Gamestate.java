1cd93b024731612318205c8b789a9291b1531fa1
VectorShadow <ncbrayshaw@hotmail.com>
2020-11-09 10:48:02 -0600

    Added FileManager class to support game state saving and loading.
    Updated IOModes affected by save and load options and changes.
    
    AbstractGameAsset is now Serializable to support saving.
    
    Floor is now Serializable to support saving.
    
    Item now extends AbstractGameAsset.

--- src/engine/Gamestate.java
+++ src/engine/Gamestate.java
@@ -76,6 +76,10 @@ public class Gamestate implements Serializable {
         instance = null;
     }
 
+    public static void clearInstance() {
+        instance = null;
+    }
+
     public static Gamestate getInstance() {
         if (instance == null) instance= new Gamestate();
         return instance;
